1e07ad1b8a54f6eecde9d9ebe3e96aaa5562d298
elyz <elyzastudy@gmail.com>
2025-06-11 20:14:31 -0300

    Initial commit

---
+++ backend/src/main/java/com/siteprofessor/backend/SiteprofessorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siteprofessor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteprofessorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteprofessorBackendApplication.class, args);
+	}
+
+}
